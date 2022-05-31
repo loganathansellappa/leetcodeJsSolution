f760f410eef3da9cc25d539e2f5a030e47fcc299
Loganathan Sellappa <loganathan.sellappa@goto.com>
2022-05-31 22:04:59 +0200

    squares-of-a-sorted-array

--- 78Subsets.js
+++ 78Subsets.js
@@ -32,3 +32,18 @@ var subsets = function(nums) {
    }
    return sets
 };
+
+
+var subsets = function(nums) {
+    let result = [];
+    dfs([], 0);
+    
+    function dfs(current, index){
+        result.push(current);
+        for(let i = index; i < nums.length; i++) {
+            dfs(current.concat(nums[i]), i + 1);
+        }
+    }
+    
+    return result;
+};
